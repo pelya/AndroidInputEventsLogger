11f37304cd40731d4861379691f14d4bab57587f
Sergii Pylypenko <x.pelya.x@gmail.com>
2013-02-08 18:11:03 +0200

    Fixed package name

--- src/AccelerometerPlayActivity.java
+++ src/AccelerometerPlayActivity.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.android.accelerometerplay.joytest;
+package com.input.events.logger.debug.app;
 
 import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
